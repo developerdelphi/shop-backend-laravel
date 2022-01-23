b78b186386111b304216568d6b38a1c99ef992a5
Oleomar Buchner <developerdelphi@gmail.com>
2022-01-23 07:11:51 -0300

    Routes: Create routes protected and public to Authenticate users

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductsController;
@@ -15,9 +16,27 @@ use App\Http\Controllers\ProductsController;
 |
 */
 
-Route::apiResource('products', ProductsController::class);
-Route::get('/products/search/{name}', [ProductsController::class, 'search'])->name('products.search');
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('signup', [AuthController::class, 'signup'])->name('signup');
+Route::post('signin', [AuthController::class, 'signin'])->name('signin');
+
+
+Route::prefix('products')->group(function () {
+    Route::get('/', [ProductsController::class, 'index'])->name('products.index');
+    Route::get('/search/{name}', [ProductsController::class, 'search'])->name('products.search');
+    Route::get('/{product}', [ProductsController::class, 'show'])->name('products.show');
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::post('signout', [AuthController::class, 'signout'])->name('signout');
+
+    Route::prefix('products')->group(function () {
+        Route::post('/create', [ProductsController::class, 'store'])->name('products.store');
+        Route::put('/{product}', [ProductsController::class, 'update'])->name('products.update');
+    });
+
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
 });

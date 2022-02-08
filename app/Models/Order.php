e4cb5c114b084c5b9d09dfc0b0049fcbefe58e47
Oleomar Buchner <developerdelphi@gmail.com>
2022-02-08 06:48:59 -0300

    Order Model: Add cast to date

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -3,14 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\softDeletes;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    use HasFactory, softDeletes;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = ['user_id', 'total', 'status'];
+
+    protected $casts = [
+        'created_at' => 'datetime:d-m-Y',
+    ];
 
     public function user()
     {
